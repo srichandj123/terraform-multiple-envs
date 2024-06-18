5257cfd1ec1810e8d814633b05d1b1ce7dc27433
srichandj123 <srichandbec@gmail.com>
2024-06-18 20:22:34 +0530

    commit 2

---
+++ locals.tf
@@ -0,0 +1,3 @@
+locals {
+  rgname = "multienv-cicd-${env}"
+}
--- variables.tf
+++ variables.tf
@@ -2,7 +2,12 @@ variable "location" {
   type    = string
   default = "Central US"
 }
-
 variable "rgname" {
+  type = string
+}
+variable "tags" {
+  type = map(string)
+}
+variable "env" {
   type = string
 }
