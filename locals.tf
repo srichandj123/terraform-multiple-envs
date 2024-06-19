c89f96f0a9653501aaa757c17e7e58f8c185b0c7
srichandj123 <srichandbec@gmail.com>
2024-06-19 23:38:27 +0530

    added the load balancer

--- locals.tf
+++ locals.tf
@@ -1,5 +1,9 @@
 locals {
   rgname   = "multienv_cicd_${var.env}"
   vnetname = "multivnet_cicd-${var.env}"
+  common_tags = {
 
+    Client      = "OCC"
+    Environment = var.env
+  }
 }
--- main.tf
+++ main.tf
@@ -2,10 +2,7 @@
 resource "azurerm_resource_group" "this" {
   name     = local.rgname
   location = var.location
-  tags = {
-    Client      = "OCC"
-    Environment = "${var.env}"
-  }
+  tags     = local.common_tags
 }
 
 resource "azurerm_virtual_network" "vnet" {
@@ -13,4 +10,13 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
   address_space       = var.addrspace
+}
+
+module "caf_lb" {
+  source         = "aztfmod/caf/azurerm//modules/networking/lb"
+  version        = "5.7.12"
+  name           = "jakka-lb"
+  resource_group = azurerm_resource_group.this.name
+  region         = azurerm_resource_group.this.location
+  # insert the 5 required variables here
 }
