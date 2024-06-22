710d3d502e431d9fb6d346fe6ebeb7442a9e6e14
srichandj123 <srichandbec@gmail.com>
2024-06-22 23:52:15 +0530

    added stg account

--- main.tf
+++ main.tf
@@ -12,11 +12,10 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = var.addrspace
 }
 
-module "caf_lb" {
-  source         = "aztfmod/caf/azurerm//modules/networking/lb"
-  version        = "5.7.12"
-  name           = "jakka-lb"
-  resource_group = azurerm_resource_group.this.name
-  region         = azurerm_resource_group.this.location
-  # insert the 5 required variables here
+resource "azurerm_storage_account" "stg" {
+  name                     = "jakkastg123tftest"
+  location                 = azurerm_resource_group.this.location
+  resource_group_name      = azurerm_resource_group.this.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
